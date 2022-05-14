699c4403ed52ecbe89228ee517b9cf19aa01339a
EC2 Default User <ec2-user@ip-172-31-93-44.ec2.internal>
2022-05-14 18:34:03 +0000

    second commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,47 @@
 class BooksController < ApplicationController
+
   def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    
+    
+    # 4. トップ画面へリダイレクト
+    redirect_to book_path(book.id)
+
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to list_path(list.id)
+
+  end
+
+  def destroy
+
+    book = Book.find(params[:id])  
+    book.destroy
+    redirect_to '/books'
+    
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
